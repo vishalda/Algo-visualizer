0d30e5daa7051f7f38c7b903579b12c9506c5625
vishal <vdavishu@gmail.com>
2022-03-31 16:31:52 +0530

    :boom: Bubble Sort Animation

--- src/SortingVisualizer/sort.js
+++ src/SortingVisualizer/sort.js
@@ -25,18 +25,31 @@ class Sort extends React.Component{
         });
     }
     
-    sort(){
+    async sort(){
         const array=this.state.array;
+        const ele=document.querySelectorAll(".div-bar")
         for(let i=0;i<array.length-1;i++){
             for(let j=0;j<array.length-1;j++){
+                ele[j].style.background = 'black';
+                ele[j+1].style.background = 'black';
                 if(array[j]>array[j+1]){
                     let temp=array[j];
                     array[j]=array[j+1];
                     array[j+1]=temp;
+                    await this.waitforme(50);
                 }
+                ele[j].style.background = 'cyan';
+                ele[j+1].style.background = 'cyan';
+                this.setState({array});
             }
         }
         this.setState({array})
+    }
+
+    waitforme(milisec) { 
+        return new Promise(resolve => { 
+            setTimeout(() => { resolve('') }, milisec); 
+        }) 
     }
 
     render() {
