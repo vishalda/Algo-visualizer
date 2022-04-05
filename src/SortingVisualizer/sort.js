cf4ce7c57b87da55c6271085a323a50eacccab22
vishal <vdavishu@gmail.com>
2022-04-05 16:46:12 +0530

    :fire: Shell Sort

--- src/SortingVisualizer/sort.js
+++ src/SortingVisualizer/sort.js
@@ -1,8 +1,7 @@
+import { wait } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 import './CSS/sort.css';
 const PrimaryColor='#00ADB5';
-const backgroundColor='#222831';
-const footerColor='rgb(23,25,28)';
 const SecondaryColor='#EEEEEE';
 
 class Sort extends React.Component{
@@ -92,6 +91,80 @@ class Sort extends React.Component{
         this.setState({array});
     }
 
+    async shellSort(arr)
+    {
+        let n = arr.length;
+        const ele=document.querySelectorAll(".div-bar")
+        for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2))
+        {
+            for (let i = gap; i < n; i += 1)
+            {
+                let temp = arr[i];
+                ele[i].style.background = `${SecondaryColor}`;
+                let j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap){
+                    ele[j].style.background = `${SecondaryColor}`;
+                    ele[j-gap].style.background = `${SecondaryColor}`;
+                    arr[j] = arr[j - gap];
+                    this.setState({array:arr});
+                    await waitforme(30);
+                    ele[j].style.background = `${PrimaryColor}`;
+                    ele[j-1].style.background = `${PrimaryColor}`;
+                }
+                arr[j] = temp;
+                this.setState({array:arr});
+                await waitforme(30);
+                ele[i].style.background = `${PrimaryColor}`;
+            }
+        }
+        this.setState({array:arr});
+    }
+    
+    // mergeSort(ele,array,l,r){
+    //     if(l>=r){
+    //         return;
+    //     }
+    //     let mid=l+parseInt((r-l)/2);
+    //     this.mergeSort(ele,array,l,mid);
+    //     this.mergeSort(ele,array,mid+1,r);
+    //     this.merge(ele,array,l,mid,r);
+    // }
+
+    // merge(ele,array,l,m,r){
+    //     let n1=m-l+1,n2=r-m;
+    //     var arr1=new Array(n1);
+    //     var arr2=new Array(n2);
+    //     for(let i=0;i<n1;i++){
+    //         arr1[i]=array[l+i];
+    //     }
+    //     for(let i=0;i<n2;i++){
+    //         arr2[i]=array[m+1+i];
+    //     }
+    //     let i=0,j=0,k=l;
+    //     while(i<n1 && j<n2){
+    //         if(arr1[i]<=arr2[j]){
+    //             array[k]=arr1[i];
+    //             i++;
+    //         }else{
+    //             array[k]=arr2[j];
+    //             j++;
+    //         }
+    //         setTimeout(startTimer,20);
+    //         this.setState({array});
+    //         k++;
+    //     }
+
+    //     while(i<n1){
+    //         array[k]=arr1[i];
+    //         i++;k++;
+    //     }
+    //     while(j<n2){
+    //         array[k]=arr2[j];
+    //         j++;k++;
+    //     }
+    //     this.setState({array});               
+    // }
+
     render() {
         const {array}=this.state;
         return(
@@ -106,6 +179,7 @@ class Sort extends React.Component{
                     <button onClick={()=>this.bubbleSort()} className="button">Bubble Sort</button>
                     <button onClick={()=>this.selectionSort()} className="button">Selection Sort</button>
                     <button onClick={()=>this.insertionSort()} className="button">Insertion Sort</button>
+                    <button onClick={()=>this.shellSort(this.state.array)} className="button">Shell Sort</button>
                 </div>
             </div>
         );
